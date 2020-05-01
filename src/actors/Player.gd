5f7e395b1fec3c5f1763e320dc08a9a5d5d44646
wilPoly <misterwrinkle@gmail.com>
2020-05-01 16:24:34 +0200

    Add pickup(item)

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,14 +1,23 @@
 extends KinematicBody2D
 
+signal picked_up
+
 export var speed := 150
 
 var velocity : Vector2
+var can_pickup := false
+var item_local : Node
+
+
+func _process(delta: float) -> void:
+	_pickup(item_local)
+
 
 func _physics_process(delta: float) -> void:
-	velocity = movement() * speed * delta
+	velocity = _movement() * speed * delta
 	move_and_collide(velocity)
 	
-func movement() -> Vector2:
+func _movement() -> Vector2:
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
@@ -16,3 +25,20 @@ func movement() -> Vector2:
 		velocity = velocity.normalized()
 		return velocity
 	return velocity
+	
+	
+func _pickup(item) -> void:
+	if can_pickup:
+		if Input.is_action_just_pressed("ui_accept"):
+			print(item.name + " picked_up")
+			emit_signal("picked_up")
+			
+
+func on_pickup_entered(item) -> void:
+	item_local = item
+	can_pickup = true
+	connect("picked_up", item, "on_pickup")
+	
+func on_pickup_exited(item) -> void:
+	can_pickup = false
+	disconnect("picked_up", item, "on_pickup")
--- src/items/BaseItem.gd
+++ src/items/BaseItem.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
-signal picked_up
 
-enum Type { JUNK, WOOD, }
+enum Type { JUNK, WOOD, METAL}
+export (Type) var item_type
+
 
 func _on_body_entered(body: Node) -> void:
-	pass # Replace with function body.
+	if body.has_method("on_pickup_entered"):
+		body.on_pickup_entered(self)
+
+
+func _on_body_exited(body: Node) -> void:
+	if body.has_method("on_pickup_exited"):
+		body.on_pickup_exited(self)
+
+func on_pickup() -> void:
+	queue_free()
